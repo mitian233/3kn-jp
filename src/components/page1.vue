7588b9c0e29db896d139c8293b844b3cea152c65
mitian233 <mitian233@yahoo.co.jp>
2023-08-06 00:45:27 +0800

    add opengraph support

--- src/components/page1.vue
+++ src/components/page1.vue
@@ -19,8 +19,12 @@ onMounted(() => {
                 <img src="../assets/logo.svg" alt="Vue logo" />
               </a>
             </div>
-            <div class="rounded-3xl text-center mt-5 underline">
-              <a href="#page2" v-on:click.prevent="smoothScroll('#page2')">次へ</a>
+            <div class="rounded-3xl text-center mt-5">
+              <a class="hover:text-blue-400 duration-500" href="#page2" v-on:click.prevent="smoothScroll('#page2')">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-down-circle inline" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                </svg>
+              </a>
             </div>
           </div>
         </transition>
